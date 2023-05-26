2653fc31a741390e97b5b48a5402f01bb42f1bb4
Akhila Gouda <84775199+goudaakhila@users.noreply.github.com>
2023-05-26 09:12:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+// Toggle the active class on the navbar when hamburger is clicked
+document.querySelector('.hamburger').addEventListener('click', function() {
+  document.querySelector('.navbar').classList.toggle('active');
+});
